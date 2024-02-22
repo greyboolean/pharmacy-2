2f0ea58f99bce2cc421bacd688f79e272fcfa40c
Grey Boolean <greyboolean@gmail.com>
2024-02-22 17:59:56 +0530

    convert userRouter

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 
 // import authRouter from "./src/routes/authRouter";
-// import userRouter from "./src/routes/userRouter";
+import userRouter from "./src/routes/userRouter";
 // import medicineRouter from "./src/routes/medicineRouter";
 // import customerRouter from "./src/routes/customerRouter";
 
@@ -18,7 +18,7 @@ app.use(cors());
 
 // Routes
 // app.use("/api/v1/auth", authRouter);
-// app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 // app.use("/api/v1/medicines", medicineRouter);
 // app.use("/api/v1/customers", customerRouter);
 
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,13 +1,13 @@
-const express = require("express");
-const userController = require("../controllers/userController");
-const authController = require("../controllers/authController");
+import express, { Router } from "express";
+import userController from "../controllers/userController";
+// import authController from "../controllers/authController";
 
-const router = express.Router();
+const router: Router = express.Router();
 
 // Protect all routes after this middleware
-router.use(authController.protect);
+// router.use(authController.protect);
 // Restrict all routes after this middleware to admin only
-router.use(authController.restrictTo("owner"));
+// router.use(authController.restrictTo("owner"));
 
 // Define routes for the root path '/'
 router
@@ -22,8 +22,8 @@ router
 	.patch(userController.updateUser);
 // .delete(userController.deleteUser);
 
-// Defie routes for soft and hard user delete
+// Define routes for soft and hard user delete
 router.route("/:id/soft").delete(userController.deleteUserSoft);
 router.route("/:id/hard").delete(userController.deleteUserHard);
 
-module.exports = router;
+export default router;
